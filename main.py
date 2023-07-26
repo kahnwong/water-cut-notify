23bb11b860b29cc9a14041673f338890f7e9f801
kahnwong <karn@karnwong.me>
2023-07-26 18:07:18 +0700

    add within area check

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
 import os
 from typing import Any
 from typing import Dict
 
+import geopandas as gpd
 import requests
 from dotenv import load_dotenv
 
@@ -23,6 +25,50 @@ def _fetch_data(latitude: float, longitude: float) -> Dict[str, Any]:
     return r[0]  # first response is closest to input coordinates (hopefully)
 
 
+def _is_within_affected_area(
+    latitude: float, longitude: float, r: Dict[str, Any]
+) -> bool:
+    coordinates = []
+    for i in r["polygons"]:
+        raw = i["coordinates"]
+        value = [[float(i["longitude"]), float(i["latitude"])] for i in raw]
+
+        coordinates.append(value)
+    # ----------------------------------------------------------------
+    result_geojson = {
+        "type": "FeatureCollection",
+        "crs": {"type": "name", "properties": {"name": "results"}},
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {"type": "Polygon", "coordinates": coordinates},
+            },
+        ],
+    }
+
+    input_geojson = {
+        "type": "FeatureCollection",
+        "crs": {"type": "name", "properties": {"name": "results"}},
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        float(os.getenv("LONGITUDE")),
+                        float(os.getenv("LATITUDE")),
+                    ],
+                },
+            },
+        ],
+    }
+    # ----------------------------------------------------------------
+    result_df = gpd.read_file(json.dumps(result_geojson), driver="GeoJSON")
+    input_df = gpd.read_file(json.dumps(input_geojson), driver="GeoJSON")
+
+    return input_df.within(result_df).to_list()[0]
+
+
 def _prepare_notification_message(r: Dict[str, Any]) -> str:
     return f"""
     area: {r['areaName']}
@@ -42,7 +88,13 @@ def _send_notification_message(message: str, ntfy_topic: str):
 
 
 if __name__ == "__main__":
-    r = _fetch_data(latitude=os.getenv("LATITUDE"), longitude=os.getenv("LONGITUDE"))
+    LATITUDE = float(os.getenv("LATITUDE"))
+    LONGITUDE = float(os.getenv("LONGITUDE"))
 
-    message = _prepare_notification_message(r)
-    _send_notification_message(message=message, ntfy_topic=os.getenv("NTFY_TOPIC"))
+    r = _fetch_data(latitude=LATITUDE, longitude=LONGITUDE)
+
+    if _is_within_affected_area(latitude=LATITUDE, longitude=LONGITUDE, r=r):
+        message = _prepare_notification_message(r)
+
+        NTFY_TOPIC = os.getenv("NTFY_TOPIC")
+        _send_notification_message(message=message, ntfy_topic=NTFY_TOPIC)
