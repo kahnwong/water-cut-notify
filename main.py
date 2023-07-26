71602214f4d71d297e19812343ce22397cd59493
kahnwong <karn@karnwong.me>
2023-07-26 14:38:21 +0700

    refactor

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import os
+from typing import Any
+from typing import Dict
 
 import requests
+from dotenv import load_dotenv
 
-# fetch data
-headers = {
-    "Host": "mobile.mwa.co.th",
-    "Accept": "*/*",
-    "Accept-Language": "en-US,en;q=0.9",
-    "User-Agent": "mwa-mobile-ios/2 CFNetwork/1408.0.4 Darwin/22.5.0",
-}
 
-url = f"https://mobile.mwa.co.th/api/mobile/no-water-running-area/latitude/{os.getenv('LATITUDE')}/longitude/{os.getenv('LONGITUDE')}"
-r = requests.get(url, headers=headers).json()
+load_dotenv()
 
-# prepare message
-r = r[0]
 
-message = f"""
-area: {r['areaName']}
-soi: {r['soi']}
-reason: {r['reason']}
+def _fetch_data(latitude: float, longitude: float) -> Dict[str, Any]:
+    headers = {
+        "Host": "mobile.mwa.co.th",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "User-Agent": "mwa-mobile-ios/2 CFNetwork/1408.0.4 Darwin/22.5.0",
+    }
 
-startDate: {r['startDate']}
-endDate: {r['endDate']}
-"""
+    url = f"https://mobile.mwa.co.th/api/mobile/no-water-running-area/latitude/{latitude}/longitude/{longitude}"
+    r = requests.get(url, headers=headers).json()
 
-# send notification
-url = f"https://ntfy.sh/{os.getenv('NTFY_TOPIC')}"
+    return r[0]  # first response is closest to input coordinates (hopefully)
 
-r = requests.post(url, data=message.encode(encoding="utf-8"))
-assert r.status_code == 200
+
+def _prepare_notification_message(r: Dict[str, Any]) -> str:
+    return f"""
+    area: {r['areaName']}
+    soi: {r['soi']}
+    reason: {r['reason']}
+
+    startDate: {r['startDate']}
+    endDate: {r['endDate']}
+    """
+
+
+def _send_notification_message(message: str, ntfy_topic: str):
+    url = f"https://ntfy.sh/{ntfy_topic}"
+
+    r = requests.post(url, data=message.encode(encoding="utf-8"))
+    assert r.status_code == 200
+
+
+if __name__ == "__main__":
+    r = _fetch_data(latitude=os.getenv("LATITUDE"), longitude=os.getenv("LONGITUDE"))
+
+    message = _prepare_notification_message(r)
+    _send_notification_message(message=message, ntfy_topic=os.getenv("NTFY_TOPIC"))
