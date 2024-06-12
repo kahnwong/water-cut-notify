ab9f39ec53b8fe768b66126b7f8698f0c520ae7d
Karn Wong <karn@karnwong.me>
2024-06-12 21:51:16 +0700

    init env

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// init env
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Loading env from env var instead...")
+	}
+
+	latitude := os.Getenv("LATITUDE")
+	longitude := os.Getenv("LONGITUDE")
+
+	// call api
+	fmt.Println(latitude, longitude)
+
 }
