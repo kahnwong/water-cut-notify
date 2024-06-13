21252322ad5ed4f485b2931d378e50355edbfe75
Karn Wong <karn@karnwong.me>
2024-06-13 07:24:08 +0700

    fix struct & use slog

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"strconv"
@@ -13,24 +14,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type NoWaterRunningArea struct {
-	Latitude             float64 `json:"latitude"`
-	Longitude            float64 `json:"longitude"`
-	GenerationtimeMs     float64 `json:"generationtime_ms"`
-	UtcOffsetSeconds     int     `json:"utc_offset_seconds"`
-	Timezone             string  `json:"timezone"`
-	TimezoneAbbreviation string  `json:"timezone_abbreviation"`
-	Elevation            float64 `json:"elevation"`
-	HourlyUnits          struct {
-		Time                     string `json:"time"`
-		Temperature2M            string `json:"temperature_2m"`
-		PrecipitationProbability string `json:"precipitation_probability"`
-	} `json:"hourly_units"`
-	Hourly struct {
-		Time                     []string  `json:"time"`
-		Temperature2M            []float64 `json:"temperature_2m"`
-		PrecipitationProbability []int     `json:"precipitation_probability"`
-	} `json:"hourly"`
+type NoWaterRunningArea []struct {
+	AreaName       string   `json:"areaName"`
+	StartDate      string   `json:"startDate"`
+	EndDate        string   `json:"endDate"`
+	Soi            string   `json:"soi"`
+	Reason         string   `json:"reason"`
+	ImpactBranches []string `json:"impactBranches"`
+	Polygons       []struct {
+		Coordinates []struct {
+			Latitude  string `json:"latitude"`
+			Longitude string `json:"longitude"`
+		} `json:"coordinates"`
+	} `json:"polygons"`
 }
 
 func stringToFloat(s string) (float64, error) {
@@ -42,7 +38,7 @@ func stringToFloat(s string) (float64, error) {
 	return vInt, err
 }
 
-func getNoWaterRunningAreaData(latitude float64, longitude float64) NoWaterRunningArea {
+func getNoWaterRunningAreaData(latitude float64, longitude float64) (NoWaterRunningArea, error) {
 	// fetch data
 	url := fmt.Sprintf("https://mobile.mwa.co.th/api/mobile/no-water-running-area/latitude/%v/longitude/%v", latitude, longitude)
 	resp, err := http.Get(url)
@@ -62,7 +58,7 @@ func getNoWaterRunningAreaData(latitude float64, longitude float64) NoWaterRunni
 		log.Println("Can not unmarshal JSON")
 	}
 
-	return result
+	return result, err
 }
 
 func main() {
@@ -72,20 +68,27 @@ func main() {
 		fmt.Println("Loading env from env var instead...")
 	}
 
+	//// latitude
 	latitudeStr := os.Getenv("LATITUDE")
 	latitude, err := stringToFloat(latitudeStr)
 	if err != nil {
 		fmt.Println("Error converting latitude to float:", err)
 	}
+	slog.Info(fmt.Sprintf("Latitude: %v", latitude))
 
+	//// longitude
 	longitudeStr := os.Getenv("LONGITUDE")
 	longitude, err := stringToFloat(longitudeStr)
 	if err != nil {
 		fmt.Println("Error converting longitude to float:", err)
 	}
+	slog.Info(fmt.Sprintf("Longitude: %v", longitude))
 
 	// call api
-	fmt.Println(latitude, longitude)
-	getNoWaterRunningAreaData(latitude, longitude)
+	r, err := getNoWaterRunningAreaData(latitude, longitude)
+	if err != nil {
+		fmt.Println("Error getting no water running area data:", err)
+	}
+	fmt.Println(r[0].AreaName)
 
 }
